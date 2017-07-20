f8712047eefcd504f44fb3cc8e5ff10fa984c3ee
Supi <supimon@gmail.com>
2017-07-20 12:34:15 +0530

    second section started

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /**
- * Created by supimon on 17/July/17.
+ * Created by supimon on 18/July/17.
  */
 
 setTimeout(function(){
@@ -10,9 +10,20 @@ setTimeout(function(){
     }).addClass('animated flipOutX');
     $('.child.right').one('animationend', function(){
         $('.child.right').removeClass('basis-0 collapse-menu').addClass('basis-97');
+        $('.hamburger').removeClass('open');
+        $('#nav-icon1').removeClass('open');
+        $('#nav-icon1 span').css("backgroundColor", "green");
     }).addClass('collapse-menu');
 
     (function(){
+        // mobile menu animation
+        $('#nav-icon1').one('click', function(){
+            menuSwipe($(this));
+        });
+        // desktop menu animation
+        $('.hamburger').one('click', function(){
+            menuSwipe($(this));
+        });
         // text animation
         $('.child.right').on('animationstart', function(){
             return function(action) {
@@ -21,14 +32,25 @@ setTimeout(function(){
                 });
             }($('.child.right').hasClass('basis-97') ? "flipInX": "flipOutX");
         });
-        // menu page swipe animation
-        $('.child.left').on('click', function(){
+        // menu page swipe utility
+        function menuSwipe($this){
             return function(action){
                 $('.child.right').addClass(action[0]).one('animationend', function(){
+                    $('.hamburger, #nav-icon1').toggleClass('open');
                     $('.child.right').removeClass(action[1]+" "+action[0]).addClass(action[2]);
+                    $('#nav-icon1 span').css("backgroundColor", action[3]);
+                    $this.one('click', function(){
+                        menuSwipe($(this));
+                    });
                 });
-            }($('.child.right').hasClass('basis-97') ? ['expand-menu', 'basis-97', 'basis-0'] :
-                ['collapse-menu', 'basis-0', 'basis-97']);
+            }($('.child.right').hasClass('basis-97') ? ['expand-menu', 'basis-97', 'basis-0', 'white'] :
+                ['collapse-menu', 'basis-0', 'basis-97', '#005d3d']);
+        }
+        // pulse animation for slider navigators
+        $('.slide-indicator-ul li a').on('mouseover', function(){
+            $(this).addClass('animated pulse').on('animationend', function(){
+                $(this).removeClass('animated pulse');
+            });
         });
     })();
 }, 2000);
